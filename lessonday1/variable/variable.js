8db49805bbcbb4ec3562606b387a1640be01ec86
Web-kids <web-kids@admin-08s-iMac.local>
2025-11-16 17:49:57 +0800

    add:lesson day 1

---
+++ lessonday1/variable/variable.js
@@ -0,0 +1,44 @@
+//var name = "eh"
+//console.log("my name is " + name)
+//console.log(`my name is ${name}`);
+
+
+//var family_members = 5;
+//console.log("my family has " + family_members);
+//var age = 13;
+//console.log("i'm " + age)
+//var school = 48;
+//console.log("i go to school " + school)
+//var hobby = "knitting";
+//console.log("i like " + hobby)
+
+//let pet ="amur"
+//console.log("my pets name is " + pet)
+//const food ="pizza"
+//console.log("i want " + food)
+
+//ex1
+
+//let nam = prompt("what's your name?")
+//let ag = prompt("how old are you?")
+//console.log("my name is " + nam)
+//console.log("i'm  " + ag)
+//let birth = prompt('where are you from?')
+//console.log("i'm from " + birth)
+//let hobb = prompt("what's your favourite hobby")
+//console.log("my favourite hobby is " + hobb)
+//let hehe = prompt("your name is " + nam)
+
+// ex2
+
+//let number = Number(prompt("first number"))
+//let number2 = Number(prompt("second number"))
+//let times = number * number2
+//console.log(times)
+
+// ex3
+
+alert("Сайн уу! Намайг Кодбот гэдэг. Би ирээдүйн програмистуудтай ярих дуртай. Чамд туслахдаа таатай байх болно.")
+let asksyourname = prompt("Эхлээд танилцъя. Чиний нэр хэн бэ?")
+let nice = prompt(`Гоё нэртэй юм! ${asksyourname}, чи ямар хэл сурахыг хамгийн их хүсэж байна?`)
+let lan = prompt(`${nice} бол гайхалтай сонголт! Чи яагаад ${nice} сурахыг хүссэн юм бэ?`)
